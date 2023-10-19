1283e891226b0bec963b263e6512204d2c845eb9
Ivo Krachunov <2kraci81@solent.ac.uk>
2023-10-19 12:42:55 +0100

    Testing PyCharm with GitHub.

---
+++ week4/Built In Functions.py
@@ -0,0 +1,43 @@
+'''
+1#
+print("Program Started")
+print("Please enter a letter")
+input("")
+# Python program to print
+# ASCII Value of Character
+
+# In c we can assign different
+# characters of which we want ASCII value
+
+c = 'g'
+# print the ASCII value of assigned character in c
+print("The ASCII value of '" + c + "' is", ord(c))
+'''
+'......................................................'
+'''
+2#
+print("Program Started")
+print("Please enter an ASCII code:")
+c = 'm'
+print("The ASCII value of '" + c + "' is", ord(c))
+print("program end")
+'''
+'......................................................'
+'''
+3#
+# The function
+print("what sound did you hear")
+rumbel = input()
+print("That was a loud rumble!")
+'''
+'......................................................'
+'4#'
+'''
+# The function
+print("What lies before us?")
+alargebouler = input()
+print("It's time to run!")
+'''
+'......................................................'
+"5#"
+print("")
